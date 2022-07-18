268ecbf8282148ff002ac65c5ebd4357a15e5d38
liaochungyi <lyle.liao@pinnov.com>
2022-07-18 23:51:16 +0800

    remove prefix and add CNAME file

--- app/utils/prefix.ts
+++ app/utils/prefix.ts
@@ -1,3 +1,4 @@
-const prefix = process.env.NODE_ENV === 'production' ? '/gama-green-energy' : '';
+// const prefix = process.env.NODE_ENV === 'production' ? '/gama-green-energy' : '';
+const prefix = process.env.NODE_ENV === 'production' ? '' : '';
 
 export { prefix };
