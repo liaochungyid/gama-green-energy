8caf99baa28daa5fbf882c27bcf2f3e44f4745e1
liaochungyi <lyle.liao@pinnov.com>
2022-11-30 20:57:16 +0800

    2022-11-30 Wang request remove serveice-03.jpg on site

--- app/components/SolutionSection.tsx
+++ app/components/SolutionSection.tsx
@@ -4,10 +4,10 @@ import theme from 'theme';
 import YouTube from 'react-youtube';
 import { ISolutionSection } from 'types';
 
-interface AdvantageCardProps {title: string; description: string}
+interface AdvantageCardProps { title: string; description: string }
 
-const AdvantageCard = ({title, description}: AdvantageCardProps) => (
-    <Grid item xs={4} sm={3} data-aos="fade-left" minHeight={{xs: 150, sm: 180, md: 200}} bgcolor='#F3F3F3' mb={4} py={{xs: 2, sm: 3}} pl={4} pr={2} position='relative' sx={{
+const AdvantageCard = ({ title, description }: AdvantageCardProps) => (
+    <Grid item xs={4} sm={3} data-aos="fade-left" minHeight={{ xs: 150, sm: 180, md: 200 }} bgcolor='#F3F3F3' mb={4} py={{ xs: 2, sm: 3 }} pl={4} pr={2} position='relative' sx={{
         '&::before': {
             content: '""',
             position: 'absolute',
@@ -31,7 +31,7 @@ const AdvantageCard = ({title, description}: AdvantageCardProps) => (
     </Grid>
 );
 
-export default function SolutionSection({ CSolutionSection }: {CSolutionSection: ISolutionSection}) {
+export default function SolutionSection({ CSolutionSection }: { CSolutionSection: ISolutionSection }) {
     const {
         title,
         titleDes,
@@ -53,11 +53,11 @@ export default function SolutionSection({ CSolutionSection }: {CSolutionSection:
                         backgroundPosition: 'center',
                         backgroundSize: 'cover',
                     }}>
-                        <Grid container py={{xs: 4, sm: 6, md: 8, lg: 10}}  px={{xs: 1, sm: 2, md: 4, lg: 8}}>
-                            <Grid item display={{xs: 'none', sm: 'block'}} sm={5} mx='auto'>
+                        <Grid container py={{ xs: 4, sm: 6, md: 8, lg: 10 }} px={{ xs: 1, sm: 2, md: 4, lg: 8 }}>
+                            <Grid item display={{ xs: 'none', sm: 'block' }} sm={5} mx='auto'>
                                 <img src={`/images/solution_title.jpg`} width='100%' alt="解決方案" />
                             </Grid>
-                            <Grid item xs={10} sm={5} mx='auto' display='flex' flexDirection='column' sx={{justifyContent: 'center'}}>
+                            <Grid item xs={10} sm={5} mx='auto' display='flex' flexDirection='column' sx={{ justifyContent: 'center' }}>
                                 <Typography variant='h2' color='info.main' mb={2.5}>{title}</Typography>
                                 <Typography variant='h5' color='common.white'>
                                     {titleDes}
@@ -67,17 +67,17 @@ export default function SolutionSection({ CSolutionSection }: {CSolutionSection:
                     </Grid>
                 </Grid>
             </Grid>
-            
+
             <Grid item maxWidth='xl' mb={7.5}>
                 <Grid container>
-                    <Grid item xs={12} px={{xs: 1, sm: 2, md: 4, lg: 8}} data-aos="fade-up">
+                    <Grid item xs={12} px={{ xs: 1, sm: 2, md: 4, lg: 8 }} data-aos="fade-up">
                         <Typography variant='h5' color='secondary.main' mb={5}>
                             {subtitleFirst}
                         </Typography>
                     </Grid>
                     <Grid item xs={12} position='relative' data-aos="fade-up">
                         <Grid container justifyContent='flex-end'>
-                            {contentFirst.map((c, i) => <AdvantageCard key={c} title={`0${i+1}.`} description={c} />)}
+                            {contentFirst.map((c, i) => <AdvantageCard key={c} title={`0${i + 1}.`} description={c} />)}
                         </Grid>
                         <Box position='absolute' width='100%' height='126px' sx={{
                             bottom: 0,
@@ -92,36 +92,41 @@ export default function SolutionSection({ CSolutionSection }: {CSolutionSection:
                         }} />
                     </Grid>
 
-                    <Grid item xs={12} mb={{xs: 5, sm: 6, md: 8, lg: 10}} px={{xs: 1, sm: 2, md: 4, lg: 8}} data-aos="fade-up">
-                        <Grid container alignItems='center' flexDirection={{xs: 'column', sm: 'row'}}>
+                    <Grid item xs={12} mb={{ xs: 5, sm: 6, md: 8, lg: 10 }} px={{ xs: 1, sm: 2, md: 4, lg: 8 }} data-aos="fade-up">
+                        <Grid container alignItems='center' flexDirection={{ xs: 'column', sm: 'row' }}>
                             <Grid item xs={12}>
                                 <Typography variant='h5' color='secondary.main' mb={2.5}>
                                     {subtitleSecond[0]}<br />
                                     {subtitleSecond[1]}
                                 </Typography>
-                                <Typography variant='subtitle2' color='common.black'>{contentSecond[0]}</Typography>
+                                <Typography variant='subtitle2' color='common.black'>{contentSecond[0]}
+                                    <br /><br />
+                                    {contentSecond[1]}
+                                    <br /><br />
+                                    {contentSecond[2]}</Typography>
                             </Grid>
                         </Grid>
                     </Grid>
 
-                    <Grid item xs={12} mb={{xs: 5, sm: 6, md: 8, lg: 10}} px={{xs: 1, sm: 2, md: 4, lg: 8}} data-aos="fade-up">
-                        <Grid container alignItems='center' flexDirection={{xs: 'column-reverse', sm: 'row'}}>
-                            <Grid item xs={12} sm={5} py={{xs: 2, sm: 0}} px={{xs: 6, sm: 2, md: 0}}>
-                                <img width='100%' height='100%' style={{objectFit: 'contain'}} src={`/images/service-03.jpg`} alt="廢棄物處理流程" srcSet="" />
+                    {/* <Grid item xs={12} mb={{ xs: 5, sm: 6, md: 8, lg: 10 }} px={{ xs: 1, sm: 2, md: 4, lg: 8 }} data-aos="fade-up">
+                        <Grid container alignItems='center' flexDirection={{ xs: 'column-reverse', sm: 'row' }}>
+                            <Grid item xs={12} sm={5} py={{ xs: 2, sm: 0 }} px={{ xs: 6, sm: 2, md: 0 }}>
+                                <img width='100%' height='100%' style={{ objectFit: 'contain' }} src={`/images/service-03.jpg`} alt="廢棄物處理流程" srcSet="" />
                             </Grid>
                             <Grid item xs={12} sm={7} md={6} ml='auto'>
+                                <Grid item xs={12} sm={7} md={6} ml='auto'>
                                 <Typography variant='subtitle2' color='common.black'>
-                                {contentSecond[1]}
-                                <br /><br />
-                                {contentSecond[2]}
+                                    {contentSecond[1]}
+                                    <br /><br />
+                                    {contentSecond[2]}
                                 </Typography>
                             </Grid>
                         </Grid>
-                    </Grid>
+                    </Grid> */}
 
-                    <Grid item xs={12} mb={{xs: 5, sm: 6, md: 8, lg: 10}} data-aos="fade-up">
+                    <Grid item xs={12} mb={{ xs: 5, sm: 6, md: 8, lg: 10 }} data-aos="fade-up">
                         <Grid container alignItems='center'>
-                            <Grid item xs={12} sm={6} py={{xs: 4, sm: 6, md: 8, lg: 10}} pl={{xs: 1, sm: 2, md: 4, lg: 8}} pr={{xs: 2, sm: 4, md: 8, lg: 15}} bgcolor='#07451A' position='relative' sx={{
+                            <Grid item xs={12} sm={6} py={{ xs: 4, sm: 6, md: 8, lg: 10 }} pl={{ xs: 1, sm: 2, md: 4, lg: 8 }} pr={{ xs: 2, sm: 4, md: 8, lg: 15 }} bgcolor='#07451A' position='relative' sx={{
                                 '&::before': {
                                     content: '""',
                                     position: 'absolute',
@@ -134,15 +139,15 @@ export default function SolutionSection({ CSolutionSection }: {CSolutionSection:
                                     transform: 'translateX(calc(-50vw + 50%))'
                                 }
                             }}>
-                                <Box display={{xs: 'block', sm: 'none'}} px={3}>
-                                    <img width='100%' height='100%' style={{objectFit: 'contain'}} src={`/images/service-02.jpg`} alt="去中心化" srcSet="" />
+                                <Box display={{ xs: 'block', sm: 'none' }} px={3}>
+                                    <img width='100%' height='100%' style={{ objectFit: 'contain' }} src={`/images/service-02.jpg`} alt="去中心化" srcSet="" />
                                 </Box>
                                 <Typography variant='subtitle2' color='common.white'>
-                                {contentSecond[3]}
+                                    {contentSecond[3]}
                                 </Typography>
                             </Grid>
-                            <Grid item xs={6} display={{xs: 'none', sm: 'block'}} ml='auto'>
-                                <img width='100%' height='100%' style={{objectFit: 'contain'}} src={`/images/service-02.jpg`} alt="去中心化" srcSet="" />
+                            <Grid item xs={6} display={{ xs: 'none', sm: 'block' }} ml='auto'>
+                                <img width='100%' height='100%' style={{ objectFit: 'contain' }} src={`/images/service-02.jpg`} alt="去中心化" srcSet="" />
                             </Grid>
                         </Grid>
                     </Grid>
@@ -151,7 +156,7 @@ export default function SolutionSection({ CSolutionSection }: {CSolutionSection:
                 </Grid>
             </Grid>
 
-            <Grid data-aos="fade-up" item width='100%' bgcolor='#F3F3F3' py={7.5} mb={{xs: 6, sm: 12, md: 18, lg: 22.5}} sx={{
+            <Grid data-aos="fade-up" item width='100%' bgcolor='#F3F3F3' py={7.5} mb={{ xs: 6, sm: 12, md: 18, lg: 22.5 }} sx={{
                 position: 'relative',
                 '&::before': {
                     [theme.breakpoints.down('sm')]: {
@@ -166,28 +171,28 @@ export default function SolutionSection({ CSolutionSection }: {CSolutionSection:
                     backgroundColor: '#FFFFFF'
                 }
             }}>
-                <Grid container maxWidth='xl' px={{xs: 1, sm: 2, md: 4, lg: 8}} width='100%' mx='auto'>
+                <Grid container maxWidth='xl' px={{ xs: 1, sm: 2, md: 4, lg: 8 }} width='100%' mx='auto'>
                     <Grid item xs={12} sm={6}>
                         <Typography variant='h5' color='secondary.main' mb={2.5}>{subtitleThird}</Typography>
-                        <Box display={{xs: 'block', sm: 'none'}} px={4}>
-                            <img width='100%' height='100%' style={{objectFit: 'contain'}} src={`/images/aurelia_device_A400.png`} alt="Aurelia A400" srcSet="" />
+                        <Box display={{ xs: 'block', sm: 'none' }} px={4}>
+                            <img width='100%' height='100%' style={{ objectFit: 'contain' }} src={`/images/aurelia_device_A400.png`} alt="Aurelia A400" srcSet="" />
                         </Box>
                         <Typography variant='subtitle2' color='common.black'>
-                        {contentThird[0]}
-                        <br /><br />
-                        {contentThird[1]}
+                            {contentThird[0]}
+                            <br /><br />
+                            {contentThird[1]}
                         </Typography>
                     </Grid>
-                    <Grid item xs={5} display={{xs: 'none', sm: 'block'}} ml='auto' zIndex={1}>
-                        <img width='100%' height='100%' style={{objectFit: 'contain'}} src={`/images/aurelia_device_A400.png`} alt="Aurelia A400" srcSet="" />
+                    <Grid item xs={5} display={{ xs: 'none', sm: 'block' }} ml='auto' zIndex={1}>
+                        <img width='100%' height='100%' style={{ objectFit: 'contain' }} src={`/images/aurelia_device_A400.png`} alt="Aurelia A400" srcSet="" />
                     </Grid>
                 </Grid>
-                    
+
                 <Grid item xs={12} mt={6}>
                     <Grid container mx='auto' justifyContent='center'>
                         <Grid item xs={10} height='0' pb='56.25%' position='relative'>
-                            <Box position='absolute' sx={{top: 0, bottom: 0, left: 0, right: 0}}>
-                                <YouTube videoId='O-lvJtYNPXM' title='Aurelia Turbines introduction of the new turbine' style={{width: '100%', height: '100%'}} opts={{width: '100%', height: '100%', playerVars: {rel: 0, modestbranding: 1}}}/>
+                            <Box position='absolute' sx={{ top: 0, bottom: 0, left: 0, right: 0 }}>
+                                <YouTube videoId='O-lvJtYNPXM' title='Aurelia Turbines introduction of the new turbine' style={{ width: '100%', height: '100%' }} opts={{ width: '100%', height: '100%', playerVars: { rel: 0, modestbranding: 1 } }} />
                             </Box>
                         </Grid>
                     </Grid>
